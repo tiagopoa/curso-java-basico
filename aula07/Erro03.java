e402b5696fb2e1508480c31c52c261e9a0ce41d4
Tiago Correa da Silva <tcv@trf4.jus.br>
2017-10-11 17:43:40 -0300

    Add aula07

---
+++ aula07/Erro03.java
@@ -0,0 +1,7 @@
+class Erro03 {
+	//essa é a entrada do programa
+	public static void main (String[] args){
+		System.out.println("Voce digitou: " + args[0]);
+		//System.out.println("Voce digitou: " + 1/0);
+	}
+}
